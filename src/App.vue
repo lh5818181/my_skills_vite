bbc2638b0355c0dc18292c6eb4f0e7e8fcbedf3e
Luis Henrique <luish7572@gmail.com>
2025-01-13 22:21:59 -0300

    todos os direitos reservados

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
       <Skills />
       <Icons />
     </div>
+    <footer class="footer">
+      <p>© Todos os direitos reservados</p>
+    </footer>
   </div>
 </template>
 
@@ -27,10 +30,22 @@ export default {
   justify-content: center;
   align-items: center;
   padding: 20px;
+  min-height: 100vh; /* Garantir que o rodapé fique no fim da página */
 }
+
 .container {
   display: flex;
   flex-direction: column;
   width: 100%;
+  flex: 1; /* Para ocupar o espaço disponível */
+}
+
+.footer {
+  text-align: center;
+    font-size: x-small;
+    width: 40%;
+    background-color: #bdc3c775;
+    border-radius: 10px;
+    color: #000;
 }
 </style>
